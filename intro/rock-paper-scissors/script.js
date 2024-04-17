5b5c5e6b0367aad734bc23052e14a3d150fea21b
tobi-ok <tobiok.cr@gmail.com>
2024-04-17 11:11:26 -0400

    Add function to run game and declare winner

--- intro/rock-paper-scissors/script.js
+++ intro/rock-paper-scissors/script.js
@@ -4,3 +4,30 @@ function getComputerChoice(){
     const n = Math.floor(Math.random() * 3)
     return choices[n]
 }
+
+function playRound(computerSelection, playerSelection){
+    if (computerSelection === playerSelection) {
+        return "Tie!"
+    }
+
+    let winner = ""
+
+    const computerSelectionIndex = choices.indexOf(computerSelection)
+    const playerSelectionIndex = choices.indexOf(playerSelection)
+
+    if (computerSelectionIndex === 0 && playerSelectionIndex === 2) {
+        winner = "computer"
+    } else if (playerSelectionIndex === 0 && computerSelectionIndex === 2) {
+        winner = 'player'
+    } else if (computerSelectionIndex >= playerSelectionIndex) {
+        winner = 'computer'
+    } else if (playerSelectionIndex >= computerSelectionIndex) {
+        winner = 'player'
+    }
+
+    if (winner === 'computer') {
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    } else {
+        return `You win! ${playerSelection} beats ${computerSelection}`
+    }
+}
