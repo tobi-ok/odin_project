729d337fbcf885ff4ae95eef111b9725dfef1d89
tobi-ok <tobiok.cr@gmail.com>
2024-04-18 15:39:07 -0400

    Add game function to run multiple games to max rounds

--- intro/rock-paper-scissors/script.js
+++ intro/rock-paper-scissors/script.js
@@ -1,4 +1,6 @@
 const choices = ['rock', 'paper', 'scissors']
+let computerWins = 0
+let playerWins = 0
 
 function getComputerChoice(){
     const n = Math.floor(Math.random() * 3)
@@ -26,8 +28,35 @@ function playRound(computerSelection, playerSelection){
     }
 
     if (winner === 'computer') {
-        return `You lose! ${computerSelection} beats ${playerSelection}`
+        computerWins++
+        return `Round lost! ${computerSelection} beats ${playerSelection} player: ${playerWins} | computer: ${computerWins}`
     } else {
-        return `You win! ${playerSelection} beats ${computerSelection}`
+        playerWins++
+        return `Round won! ${playerSelection} beats ${computerSelection}! player: ${playerWins} | computer: ${computerWins}`
     }
 }
+
+function playGame(){
+    const rounds = [
+        console.log(playRound(getComputerChoice(), prompt('Enter move ("rock", "paper", "scissors")'))),
+        console.log(playRound(getComputerChoice(), prompt('Enter move ("rock", "paper", "scissors")'))),
+        console.log(playRound(getComputerChoice(), prompt('Enter move ("rock", "paper", "scissors")'))),
+        console.log(playRound(getComputerChoice(), prompt('Enter move ("rock", "paper", "scissors")'))),
+        console.log(playRound(getComputerChoice(), prompt('Enter move ("rock", "paper", "scissors")')))
+    ]
+    
+    let gameOverText = 'Game over! '
+
+    if (computerWins > playerWins) {
+        gameOverText += 'You lose! ' 
+    } else if (playerWins > computerWins) {
+        gameOverText += 'You win! '
+    } else {
+        gameOverText += 'Tie! '
+    }
+
+    gameOverText += `player: ${playerWins} | computer: ${computerWins}`
+    console.log(gameOverText)
+}
+
+playGame()
