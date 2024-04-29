6e25a069f26deff5f32cea14d2a1a119374d603f
tobi-ok <tobiok.cr@gmail.com>
2024-04-29 15:24:13 -0400

    Add UI elements and modified game round functions
    Adds three html buttons to click on to start a round. Each round is
    counted toward a total when either the player or the computer wins 5
    rounds total.

--- intro/rock-paper-scissors/script.js
+++ intro/rock-paper-scissors/script.js
@@ -1,62 +1,81 @@
 const choices = ['rock', 'paper', 'scissors']
-let computerWins = 0
-let playerWins = 0
+let roundsPlayed = 0;
+let computerWins = 0;
+let playerWins = 0;
+
+const resultsDiv = document.querySelector(".results");
+const rockBtn = document.querySelector(".rock");
+const paperBtn = document.querySelector(".paper");
+const scissorsBtn = document.querySelector(".scissors");
 
 function getComputerChoice(){
-    const n = Math.floor(Math.random() * 3)
-    return choices[n]
+    const n = Math.floor(Math.random() * 3);
+    return choices[n];
 }
 
 function playRound(computerSelection, playerSelection){
     if (computerSelection === playerSelection) {
-        return "Tie!"
+        return "Tie!";
     }
 
-    let winner = ""
+    let winner = "";
 
-    const computerSelectionIndex = choices.indexOf(computerSelection)
-    const playerSelectionIndex = choices.indexOf(playerSelection)
+    const computerSelectionIndex = choices.indexOf(computerSelection);
+    const playerSelectionIndex = choices.indexOf(playerSelection);
 
     if (computerSelectionIndex === 0 && playerSelectionIndex === 2) {
-        winner = "computer"
+        winner = "computer";
     } else if (playerSelectionIndex === 0 && computerSelectionIndex === 2) {
-        winner = 'player'
+        winner = 'player';
     } else if (computerSelectionIndex >= playerSelectionIndex) {
-        winner = 'computer'
+        winner = 'computer';
     } else if (playerSelectionIndex >= computerSelectionIndex) {
-        winner = 'player'
+        winner = 'player';
     }
 
     if (winner === 'computer') {
-        computerWins++
-        return `Round lost! ${computerSelection} beats ${playerSelection} player: ${playerWins} | computer: ${computerWins}`
+        computerWins++;
+        return `Round lost! ${computerSelection} beats ${playerSelection} player: ${playerWins} | computer: ${computerWins}`;
     } else {
-        playerWins++
-        return `Round won! ${playerSelection} beats ${computerSelection}! player: ${playerWins} | computer: ${computerWins}`
+        playerWins++;
+        return `Round won! ${playerSelection} beats ${computerSelection}! player: ${playerWins} | computer: ${computerWins}`;
     }
 }
 
-function playGame(){
-    const rounds = [
+function playGame(playerChoice){
+    /*const rounds = [
         console.log(playRound(getComputerChoice(), prompt('Enter move ("rock", "paper", "scissors")'))),
         console.log(playRound(getComputerChoice(), prompt('Enter move ("rock", "paper", "scissors")'))),
         console.log(playRound(getComputerChoice(), prompt('Enter move ("rock", "paper", "scissors")'))),
         console.log(playRound(getComputerChoice(), prompt('Enter move ("rock", "paper", "scissors")'))),
         console.log(playRound(getComputerChoice(), prompt('Enter move ("rock", "paper", "scissors")')))
-    ]
-    
-    let gameOverText = 'Game over! '
+    ]*/
 
-    if (computerWins > playerWins) {
-        gameOverText += 'You lose! ' 
-    } else if (playerWins > computerWins) {
-        gameOverText += 'You win! '
-    } else {
-        gameOverText += 'Tie! '
+    roundsPlayed++
+    const computerChoice = getComputerChoice();
+
+    console.log(`You choose ${playerChoice}, computer choose ${computerChoice}\n` + playRound(computerChoice, playerChoice));
+
+    if (playerWins >= 5 || computerWins >= 5){
+        let gameOverText = 'Game over! ';
+
+        if (computerWins > playerWins) {
+            gameOverText += 'You lose! '; 
+        } else if (playerWins > computerWins) {
+            gameOverText += 'You win! ';
+        } else {
+            gameOverText += 'Tie! ';
+        }
+
+        gameOverText += `player: ${playerWins} | computer: ${computerWins}\nRounds played: ${roundsPlayed}`;
+        console.log(gameOverText);
+
+        roundsPlayed = 0;
+        playerWins = 0;
+        computerWins = 0;
     }
-
-    gameOverText += `player: ${playerWins} | computer: ${computerWins}`
-    console.log(gameOverText)
 }
 
-playGame()
+rockBtn.addEventListener("click", () => {playGame("rock")})
+paperBtn.addEventListener("click", () => {playGame("paper")})
+scissorsBtn.addEventListener("click", () => {playGame("scissors")})
