8eb15e45067f84be6beedb7340a35aa6fbe379d8
tobi-ok <tobiok.cr@gmail.com>
2024-05-11 17:23:03 -0400

    Add 16x16 grid using javascript

---
+++ intro/etch-a-sketch/script.js
@@ -0,0 +1,28 @@
+const GRID_SIZE = 16
+
+const container = document.createElement("div");
+container.setAttribute("class", 'container');
+container.style.display = "flex"; 
+container.style.height = "100%";
+//container.style.flexWrap = "Wrap";
+container.style.flexDirection = "column";
+
+for (let y = 0; y < GRID_SIZE; y++){
+    const rowContainer = document.createElement("div");
+    rowContainer.style.backgroundColor = "Orange";
+    rowContainer.style.height = "45px";
+    rowContainer.style.display = "flex";
+    
+    for (let x = 0; x < GRID_SIZE; x++) {
+        const newDiv = document.createElement('div');
+        newDiv.style.flex = "1"
+        //newDiv.style.width = `${parseInt(rowContainer.style.width)/GRID_SIZE}px`;
+        newDiv.style.backgroundColor = "Pink";
+
+        rowContainer.appendChild(newDiv)
+    }
+
+    container.appendChild(rowContainer)
+}
+
+document.body.appendChild(container)
