75803f2ebf09f68cd6bfd6b80ba4648bac2b36e1
tobi-ok <tobiok.cr@gmail.com>
2024-05-15 12:51:53 -0400

    Add style to homepage

--- intro/etch-a-sketch/script.js
+++ intro/etch-a-sketch/script.js
@@ -1,32 +1,65 @@
-const GRID_SIZE = 16
+const DEFAULT_GRID_SIZE = 16
 
-const container = document.createElement("div");
-container.setAttribute("class", 'container');
-container.style.display = "flex"; 
-container.style.height = "100%";
-//container.style.flexWrap = "Wrap";
-container.style.flexDirection = "column";
+document.body.style.display = "flex";
+document.body.style.alignItems = "center";
+document.body.style.flexDirection = "column";
 
-for (let y = 0; y < GRID_SIZE; y++){
-    const rowContainer = document.createElement("div");
-    rowContainer.style.backgroundColor = "Orange";
-    rowContainer.style.height = "45px";
-    rowContainer.style.display = "flex";
-    
-    for (let x = 0; x < GRID_SIZE*2; x++) {
-        const newDiv = document.createElement('div');
-        newDiv.style.flex = "1"
-        //newDiv.style.width = `${parseInt(rowContainer.style.width)/GRID_SIZE}px`;
-        newDiv.style.backgroundColor = "Pink";
+const pageTitle = document.createElement("div");
+pageTitle.textContent = "Etch-A-Sketch";
+pageTitle.style.textAlign = "center";
+pageTitle.style.fontWeight = "bolder";
+pageTitle.style.fontSize = "50px";
+pageTitle.width = "100%";
 
-        newDiv.addEventListener("mouseover", () => {
-            newDiv.style.backgroundColor = "black"          
-        })
+document.body.appendChild(pageTitle);
 
-        rowContainer.appendChild(newDiv)
+function createNewGrid(customGridSize){
+    let container = document.querySelector(".container") 
+    if (container){
+        while (container.hasChildNodes()){
+            container.removeChild(container.lastChild)
+        }
+    } else {
+        container = document.createElement("div");
+        container.setAttribute("class", 'container');
+        container.style.display = "flex";
+        container.style.height = "960px";
+        container.style.width = "960px";
+        container.style.flexDirection = "column";
+        container.style.border = "5px solid black";
     }
 
-    container.appendChild(rowContainer)
+    let gridSize = customGridSize ? (customGridSize) : DEFAULT_GRID_SIZE
+
+    for (let y = 0; y < gridSize; y++){
+        const rowContainer = document.createElement("div");
+        rowContainer.style.flex = "1 1 45px";
+        rowContainer.style.display = "flex";
+        
+        for (let x = 0; x < gridSize*1.5; x++) {
+            const newDiv = document.createElement('div');
+            newDiv.style.flex = "1"
+
+            newDiv.addEventListener("mouseover", () => {
+                newDiv.style.backgroundColor = "black"          
+            })
+
+            rowContainer.appendChild(newDiv)
+        }
+
+        container.appendChild(rowContainer)
+    }
+
+    document.body.appendChild(container)
 }
 
-document.body.appendChild(container)
+const newGridBtn = document.createElement("button");
+newGridBtn.textContent = "New Grid";
+newGridBtn.style.margin = "10px 0px";
+
+newGridBtn.addEventListener("click", () => {
+    const newGridSize = prompt("Enter new grid size (x): ") 
+    createNewGrid(newGridSize)
+})
+
+document.body.appendChild(newGridBtn);
